bccd39878fd32835023b9b3014999e095e7fb3ca
Alexander Travkin <aleverdes.alex@gmail.com>
2022-10-22 16:47:00 +0300

    Fixed OneFrameEx (this EcsSystems to this IEcsSystems)

--- Scripts/OneFrame/OneFrameEx.cs
+++ Scripts/OneFrame/OneFrameEx.cs
@@ -4,7 +4,7 @@ namespace AffenCode
 {
     public static class OneFrameEx
     {
-        public static EcsSystems OneFrame<T>(this EcsSystems ecsSystems) where T : struct
+        public static IEcsSystems OneFrame<T>(this IEcsSystems ecsSystems) where T : struct
         {
             ecsSystems.Add(new OneFrameSystem<T>());
             return ecsSystems;
