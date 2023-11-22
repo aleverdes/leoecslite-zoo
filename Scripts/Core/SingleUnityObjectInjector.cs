662592cbfc5eda6914d043af1504623ecb584f04
Alexander Travkin <aleverdes.alex@gmail.com>
2023-11-22 12:34:33 +0300

    Fixed invalid cast for autofilling

--- Scripts/Core/SingleUnityObjectInjector.cs
+++ Scripts/Core/SingleUnityObjectInjector.cs
@@ -87,7 +87,11 @@ namespace AleVerDes.LeoEcsLiteZoo
             {
 #if UNITY_EDITOR
                 if (PrefabStageUtility.GetCurrentPrefabStage() != null)
-                    return PrefabStageUtility.GetCurrentPrefabStage().FindComponentsOfType<Transform>().FirstOrDefault(x => x.GetComponent(componentType));
+                    return PrefabStageUtility
+                        .GetCurrentPrefabStage()
+                        .FindComponentsOfType<Transform>()
+                        .FirstOrDefault(x => x.GetComponent(componentType))?
+                        .GetComponent(componentType);
 #endif
                 return FindObjectOfType(componentType);
             }
