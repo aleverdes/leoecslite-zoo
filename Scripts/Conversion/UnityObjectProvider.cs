cdaabbe95a3a42eb86842875011ea40e622a2680
Alexander Travkin <aleverdes.alex@gmail.com>
2023-11-24 18:45:36 +0300

    Added ObjectRef<T> instead existing *Ref components and UnityRef<T>

--- Scripts/Conversion/UnityObjectProvider.cs
+++ Scripts/Conversion/UnityObjectProvider.cs
@@ -9,17 +9,28 @@ namespace AleVerDes.LeoEcsLiteZoo
         public void ConvertToEntity(EcsWorld world, int entity)
         {
             world.GetPool<TransformRef>().Add(entity).Value = transform;
+            world.GetPool<ObjectRef<Transform>>().Add(entity).Value = transform;
 
             if (transform is RectTransform rectTransform)
+            {
                 world.GetPool<RectTransformRef>().Add(entity).Value = rectTransform;
+                world.GetPool<ObjectRef<RectTransform>>().Add(entity).Value = rectTransform;
+            }
 
             world.GetPool<GameObjectRef>().Add(entity).Value = gameObject;
-            
-            if (TryGetComponent<Rigidbody>(out var rb)) 
+            world.GetPool<ObjectRef<GameObject>>().Add(entity).Value = gameObject;
+
+            if (TryGetComponent<Rigidbody>(out var rb))
+            {
                 world.GetPool<RigidbodyRef>().Add(entity).Value = rb;
+                world.GetPool<ObjectRef<Rigidbody>>().Add(entity).Value = rb;
+            }
 
             if (TryGetComponent<Rigidbody2D>(out var rb2d))
+            {
                 world.GetPool<Rigidbody2DRef>().Add(entity).Value = rb2d;
+                world.GetPool<ObjectRef<Rigidbody2D>>().Add(entity).Value = rb2d;
+            }
         }
     }
 }
--- Scripts/Core/EcsComponents.cs
+++ Scripts/Core/EcsComponents.cs
@@ -5,37 +5,49 @@ using UnityEngine;
 namespace AleVerDes.LeoEcsLiteZoo
 {
     [Serializable]
+    [Obsolete("Use ObjectRef<T> instead")]
     public struct TransformRef
     {
         public Transform Value;
     }
 
     [Serializable]
+    [Obsolete("Use ObjectRef<T> instead")]
     public struct RectTransformRef
     {
         public RectTransform Value;
     }
     
     [Serializable]
+    [Obsolete("Use ObjectRef<T> instead")]
     public struct RigidbodyRef
     {
         public Rigidbody Value;
     }
 
     [Serializable]
+    [Obsolete("Use ObjectRef<T> instead")]
     public struct Rigidbody2DRef
     {
         public Rigidbody2D Value;
     }
 
     [Serializable]
+    [Obsolete("Use ObjectRef<T> instead")]
     public struct GameObjectRef
     {
         public GameObject Value;
     }
 
     [Serializable]
+    [Obsolete("Use ObjectRef<T> instead")]
     public struct UnityRef<T> where T : UnityEngine.Object
+    {
+        public T Value;
+    }
+
+    [Serializable]
+    public struct ObjectRef<T> where T : UnityEngine.Object
     {
         public T Value;
     }
