4ea5fddac5adb51ebe7cf7a29e60b1b37c9850d7
Alexander Travkin <aleverdes.alex@gmail.com>
2024-05-05 09:49:48 +0300

    Minor zenject extensions update

--- Scripts/Extensions/ZenjectEx.cs
+++ Scripts/Extensions/ZenjectEx.cs
@@ -6,24 +6,18 @@ namespace AleVerDes.LeoEcsLiteZoo
 {
     public static class ZenjectEx
     {
-        public static IfNotBoundBinder BindNewEcsWorldFor<T>(this DiContainer binder) where T : IEcsModule
+        public static void BindNewEcsWorldFor<T>(this DiContainer binder) where T : IEcsModule
         {
             binder.BindInterfacesAndSelfTo<EcsWorld>().AsSingle().NonLazy();
-            return binder.WithEcsRunnerFor<T>().NonLazy();
+            binder.WithEcsRunnerFor<T>().NonLazy();
         }
         
         private static ConditionCopyNonLazyBinder WithEcsRunnerFor<T>(this DiContainer binder) where T : IEcsModule
         {
-            return binder.BindInterfacesAndSelfTo<EcsRunner>().AsSingle().OnInstantiated((context, ecsManager) =>
-            {
-                if (ecsManager is EcsRunner manager)
-                    manager.InstallModule(context.Container.Instantiate<T>());
-                else
-                    throw new Exception("EcsManager is not found in the container.");
-            });
+            return binder.BindInterfacesAndSelfTo<EcsRunner>().WithEcsModule<T>();
         }
-        
-        public static ConditionCopyNonLazyBinder WithEcsModule<T>(this FromBinderNonGeneric binder) where T : IEcsModule
+
+        private static ConditionCopyNonLazyBinder WithEcsModule<T>(this ScopeConcreteIdArgConditionCopyNonLazyBinder binder) where T : IEcsModule
         {
             return binder.AsSingle().OnInstantiated((context, ecsManager) =>
             {
